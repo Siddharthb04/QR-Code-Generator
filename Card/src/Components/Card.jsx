d4361fcf909b3957de0d23a53860b70eb7a12afb
SANJAY A <118290917+Sanjay2729117@users.noreply.github.com>
2024-07-21 22:06:10 +0530

    Update Card.jsx

--- Card/src/Components/Card.jsx
+++ Card/src/Components/Card.jsx
@@ -52,7 +52,7 @@ const Card = () => {
     <button onClick={generate} className='generate' disabled={loading}>Generate QR Code</button>
     <button className='Download' onClick={download}>Download QR Code</button>
     
-   <a className="footer">Designed by Sanjay</a>
+   <a className="footer">Designed with ❤ by <span> Sanjay </span></a>
    </div>
    
    </div>
